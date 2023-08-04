e33553318922379db50467d54e7c851e93d0c4b6
raymond12345612 <min5533552@gmail.com>
2023-08-04 15:22:44 -0400

    raymond

---
+++ src/VirtualPetTick.java
@@ -0,0 +1,16 @@
+public class VirtualPetTick {
+    // this is for organic pet tick
+    public void organicPetTick(){
+        hunger += 10;
+        thirst += 10;
+        boredom -= 10;
+        health += 5;
+        sleepy -= 5;
+    }
+    // this is for robotic pet tick
+    public void roboticPetTick(){
+        energy -= 10;
+        rusty -= 5;
+        oil -= 10;
+    }     
+}
