0768ec895a5db824f102c6ffa218ac148f16ab57
Y27MG <yudithmarte@gmail.com>
2023-08-04 16:44:52 -0400

    first commit

--- src/Cat.java
+++ src/Cat.java
@@ -7,6 +7,18 @@ public class Cat extends Animal {
 
     }
 
+    public void Meow() {
+        System.out.println("*MEOW*");
+    }
+
+    public void Feed() {
+        System.out.println("*PURRR*");
+    }
+
+    public void Thirst() {
+        System.out.println("*GULP,GULP*");
+    }
+
     @Override
     public String toString() {
         return name + " is your cats name.";
--- src/Dog.java
+++ src/Dog.java
@@ -9,13 +9,25 @@ public class Dog extends Animal {
 
     }
 
+    public void feed() {
+        System.out.println("*WOOF,WOOF,WOOF!*");
+    }
+
+    public void Thirst() {
+        System.out.println("*GULP,GULP*");
+    }
+    public void Abandon(){
+        System.out.println("Aauuuu!");
+    } 
+
     public String getType() {
         return type;
     }
 
-    @Override 
-    public   String toString(){
-        return "Your animals name is " + name + " and they are a " + species + " species.  You have chosen a " +type+" dog.";
+    @Override
+    public String toString() {
+        return "Your animals name is " + name + " and they are a " + species + " species.  You have chosen a " + type
+                + " dog.";
 
     }
 }
